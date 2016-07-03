d379fa40a66e75cd096840b8abecdf9b5b57a697
Elyse Erb <jerb@me.com>
2016-07-03 16:31:17 -0700

    Checkpoint 19 Complete

---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Elyse Erb on 7/3/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
