48a836ce91c661da5582505b7fc6b2d76edb2943
Elyse Erb <jerb@me.com>
2016-07-03 21:09:09 -0700

    Checkpoint 20 Completed

---
+++ Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Elyse Erb on 7/3/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
