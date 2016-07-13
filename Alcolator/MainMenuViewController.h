259b28b4b7ef730fe0ed847d438bb66ecb679e7c
Elyse Erb <jerb@me.com>
2016-07-13 16:29:40 -0700

    Still having launching issues

---
+++ Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Elyse Erb on 7/13/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
